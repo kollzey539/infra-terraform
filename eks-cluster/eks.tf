4fcbd33b7d8de4eaf43b7082dd5d3e6d788171d2
kollzey539 <kollzey539@gmail.com>
2025-05-11 20:42:13 +0000

    increasing node size count

--- eks-cluster/eks.tf
+++ eks-cluster/eks.tf
@@ -21,9 +21,9 @@ resource "aws_eks_node_group" "node_group_a" {
   instance_types  = ["t3.large"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   ami_type = var.ami_type

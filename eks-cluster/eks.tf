996144cd6402cfb550ff337297dfaa3624af7bb3
kollzey539 <kollzey539@gmail.com>
2025-06-06 14:40:26 -0500

    Update eks.tf

--- eks-cluster/eks.tf
+++ eks-cluster/eks.tf
@@ -22,7 +22,7 @@ resource "aws_eks_node_group" "node_group_a" {
 
   scaling_config {
     desired_size = 0
-    max_size     = 0
+    max_size     = 1
     min_size     = 0
   }
 
@@ -44,7 +44,7 @@ resource "aws_eks_node_group" "node_group_b" {
 
   scaling_config {
     desired_size = 0
-    max_size     = 0
+    max_size     = 1
     min_size     = 0
   }
 
